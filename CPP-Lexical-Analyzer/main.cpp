35139e8ae48fbc82c69775f4d00bede7e12add29
BoogiemanNSK <calah47@yandex.ru>
2019-09-02 20:44:37 +0300

    Reading and writing file with errors handling

--- CPP-Lexical-Analyzer/main.cpp
+++ CPP-Lexical-Analyzer/main.cpp
@@ -5,16 +5,24 @@
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Usage: " EXE_NAME ".exe [Source file]\n";
+        std::cout << "[ERROR] Usage: " EXE_NAME ".exe [Source file]\n";
         return -1;
     }
 
-    std::ifstream in;
-    in.open(argv[1]); // TODO Обработать ошибку неправильного имени файла
+    std::ifstream in(argv[1]);
+    if (!in.is_open()) {
+        std::cout << "[ERROR] Incorrect source file name!\n";
+        return -1;
+    }
 
-    
+    char ch = '0';    
+    while(ch != EOF) {
+        ch = in.get();
+        if (ch == EOF) continue;
+
+        std::cout << ch;
+    }
 
     in.close();
-
     return 0;
 }
