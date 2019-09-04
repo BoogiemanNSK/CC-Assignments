94fa923877d52e86b4b374dec42f6e3eff6347a2
BoogiemanNSK <calah47@yandex.ru>
2019-09-04 17:00:43 +0300

    Completed yet untested analyzer

--- CPP-Lexical-Analyzer/main.cpp
+++ CPP-Lexical-Analyzer/main.cpp
@@ -152,8 +152,8 @@ int main(int argc, char** argv) {
                     suffix += tolower(ch);
                     ch = in.get();
                 }
-
-                if (isEndOfLiteral(ch) && isCorrectDecimalSuffix(suffix)) {
+                
+                if (isCorrectDecimalSuffix(suffix)) {
                     if (suffix == "") /* LITERAL_HEX */;
                     else if (suffix == "u") /* LITERAL_UNSIGNED_HEX */;
                     else if (suffix == "l") /* LITERAL_LONG_HEX */;
@@ -179,7 +179,7 @@ int main(int argc, char** argv) {
                     ch = in.get();
                 }
 
-                if (isEndOfLiteral(ch) && isCorrectDecimalSuffix(suffix)) {
+                if (isCorrectDecimalSuffix(suffix)) {
                     if (suffix == "") /* LITERAL_BINARY */;
                     else if (suffix == "u") /* LITERAL_UNSIGNED_BINARY */;
                     else if (suffix == "l") /* LITERAL_LONG_BINARY */;
@@ -207,7 +207,7 @@ int main(int argc, char** argv) {
                     }
 
                     // Octal
-                    if (isEndOfLiteral(ch) && isCorrectDecimalSuffix(suffix)) {
+                    if (isCorrectDecimalSuffix(suffix)) {
                         if (suffix == "") /* LITERAL_OCTAL */;
                         else if (suffix == "u") /* LITERAL_UNSIGNED_OCTAL */;
                         else if (suffix == "l") /* LITERAL_LONG_OCTAL */;
@@ -215,7 +215,7 @@ int main(int argc, char** argv) {
                         else if (suffix == "ull" || suffix == "lul" || suffix == "llu") /* LITERAL_UNSIGNED_LONG_LONG_OCTAL */;
                     }
 
-                    // Mistake - ignore it
+                    // Mistake - ignore literal
                     else continue;
                 }
 
@@ -234,8 +234,8 @@ int main(int argc, char** argv) {
                         ch = in.get();
                     }
 
-                    // Octal
-                    if (isEndOfLiteral(ch) && isCorrectDecimalSuffix(suffix)) {
+                    // Integer
+                    if (isCorrectDecimalSuffix(suffix)) {
                         if (suffix == "") /* LITERAL_INT */;
                         else if (suffix == "u") /* LITERAL_UNSIGNED_INT */;
                         else if (suffix == "l") /* LITERAL_LONG_INT */;
@@ -243,10 +243,80 @@ int main(int argc, char** argv) {
                         else if (suffix == "ull" || suffix == "lul" || suffix == "llu") /* LITERAL_UNSIGNED_LONG_LONG_INT */;
                     }
 
-                    // Mistake - ignore it
+                    // Mistake - ignore literal
                     else continue;
                 }
 
+                // Decimal point
+                if (ch == '.') {
+                    str += ch;
+                    ch = in.get();
+
+                    while (ch >= '0' && ch <= '9') {
+                        str += ch;
+                        ch = in.get();
+                    }
+
+                    if (ch == 'f' || ch == 'F') {
+                        str += ch;
+                        ch = in.get();
+                        if (isEndOfLiteral(ch)) /* LITERAL_FLOAT */;                        
+                    }
+
+                    else if (ch == 'l' || ch == 'L') {
+                        str += ch;
+                        ch = in.get();
+                        if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE */;
+                    }
+
+                    else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE */;
+                }
+
+                // Exponent
+                if (ch == 'e' || ch == 'E') {
+                    str += ch;
+                    ch = in.get();
+
+                    if (ch == '+' || ch == '-') {
+                        str += ch;
+                        ch = in.get();
+                    }
+
+                    // Must be at least one number
+                    if (!(ch >= '0' && ch <= '9')) {
+                        // Otherwise, mistake - skip literal
+                        while (!isEndOfLiteral(ch) && ch != EOF) {
+                            ch = in.get();
+                        }
+                        continue;
+                    }
+
+                    while (ch >= '0' && ch <= '9') {
+                        str += ch;
+                        ch = in.get();
+                    }
+
+                    if (ch == 'f' || ch == 'F') {
+                        str += ch;
+                        ch = in.get();
+                        if (isEndOfLiteral(ch)) /* LITERAL_FLOAT_WITH_EXP */;                        
+                    }
+
+                    else if (ch == 'l' || ch == 'L') {
+                        str += ch;
+                        ch = in.get();
+                        if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE_WITH_EXP */;
+                    }
+
+                    else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE_WITH_EXP */;
+                }
+
+                // If reached this line - then mistake
+                // Not octal, integer or floating-point number
+                // Skip literal
+                while (!isEndOfLiteral(ch) && ch != EOF) {
+                    ch = in.get();
+                }
             }
 
             // Just a zero integer
@@ -256,9 +326,188 @@ int main(int argc, char** argv) {
 
         // Integer / Floating-point
         else if (ch >= '1' && ch <= '9') {
+            str += ch;
+            ch = in.get();
 
+            // Integer check
+            while (ch >= '0' && ch <= '9') {
+                str += ch;
+                ch = in.get();
+            }
+
+            // Either integer or mistake
+            if (ch == 'l' || ch == 'L' || ch == 'u' || ch == 'U' || isEndOfLiteral(ch)) {
+                std::string suffix = "";
+                while (!isEndOfLiteral(ch) && ch != EOF) {
+                    str += ch;
+                    suffix += tolower(ch);
+                    ch = in.get();
+                }
+
+                // Integer
+                if (isCorrectDecimalSuffix(suffix)) {
+                    if (suffix == "") /* LITERAL_INT */;
+                    else if (suffix == "u") /* LITERAL_UNSIGNED_INT */;
+                    else if (suffix == "l") /* LITERAL_LONG_INT */;
+                    else if (suffix == "lu" || suffix == "ul") /* LITERAL_UNSIGNED_LONG_INT */;
+                    else if (suffix == "ull" || suffix == "lul" || suffix == "llu") /* LITERAL_UNSIGNED_LONG_LONG_INT */;
+                }
+
+                // Mistake - ignore literal
+                else continue;
+            }
+
+            // Decimal point
+            if (ch == '.') {
+                str += ch;
+                ch = in.get();
+
+                while (ch >= '0' && ch <= '9') {
+                    str += ch;
+                    ch = in.get();
+                }
+
+                if (ch == 'f' || ch == 'F') {
+                    str += ch;
+                    ch = in.get();
+                    if (isEndOfLiteral(ch)) /* LITERAL_FLOAT */;                        
+                }
+
+                else if (ch == 'l' || ch == 'L') {
+                    str += ch;
+                    ch = in.get();
+                    if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE */;
+                }
+
+                else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE */;
+            }
+
+            // Exponent
+            if (ch == 'e' || ch == 'E') {
+                str += ch;
+                ch = in.get();
+
+                if (ch == '+' || ch == '-') {
+                    str += ch;
+                    ch = in.get();
+                }
+
+                // Must be at least one number
+                if (!(ch >= '0' && ch <= '9')) {
+                    // Otherwise, mistake - skip literal
+                    while (!isEndOfLiteral(ch) && ch != EOF) {
+                        ch = in.get();
+                    }
+                    continue;
+                }
+
+                while (ch >= '0' && ch <= '9') {
+                    str += ch;
+                    ch = in.get();
+                }
+
+                if (ch == 'f' || ch == 'F') {
+                    str += ch;
+                    ch = in.get();
+                    if (isEndOfLiteral(ch)) /* LITERAL_FLOAT_WITH_EXP */;                        
+                }
+
+                else if (ch == 'l' || ch == 'L') {
+                    str += ch;
+                    ch = in.get();
+                    if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE_WITH_EXP */;
+                }
+
+                else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE_WITH_EXP */;
+            }
+
+            // If reached this line - then mistake
+            // Not octal, integer or floating-point number
+            // Skip literal
+            while (!isEndOfLiteral(ch) && ch != EOF) {
+                ch = in.get();
+            }
         }
 
+        // Floating-point with no integer part
+        else if (ch == '.') {
+            str += ch;
+            ch = in.get();
+
+            while (ch >= '0' && ch <= '9') {
+                str += ch;
+                ch = in.get();
+            }
+
+            if (ch == 'f' || ch == 'F') {
+                str += ch;
+                ch = in.get();
+                if (isEndOfLiteral(ch)) /* LITERAL_FLOAT */;                        
+            }
+
+            else if (ch == 'l' || ch == 'L') {
+                str += ch;
+                ch = in.get();
+                if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE */;
+            }
+
+            else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE */;
+
+            // Exponent
+            else if (ch == 'e' || ch == 'E') {
+                str += ch;
+                ch = in.get();
+
+                if (ch == '+' || ch == '-') {
+                    str += ch;
+                    ch = in.get();
+                }
+
+                // Must be at least one number
+                if (!(ch >= '0' && ch <= '9')) {
+                    // Otherwise, mistake - skip literal
+                    while (!isEndOfLiteral(ch) && ch != EOF) {
+                        ch = in.get();
+                    }
+                    continue;
+                }
+
+                while (ch >= '0' && ch <= '9') {
+                    str += ch;
+                    ch = in.get();
+                }
+
+                if (ch == 'f' || ch == 'F') {
+                    str += ch;
+                    ch = in.get();
+                    if (isEndOfLiteral(ch)) /* LITERAL_FLOAT_WITH_EXP */;                        
+                }
+
+                else if (ch == 'l' || ch == 'L') {
+                    str += ch;
+                    ch = in.get();
+                      if (isEndOfLiteral(ch)) /* LITERAL_LONG_DOUBLE_WITH_EXP */;
+                }
+
+                else if (isEndOfLiteral(ch)) /* LITERAL_DOUBLE_WITH_EXP */;
+            }
+
+            // Should not be anything after exponent part (mistake)
+            while (!isEndOfLiteral(ch) && ch != EOF) {
+                ch = in.get();
+            }
+        }
+
+        // Additional check for bool and nullptr
+        else {
+            while (!isEndOfLiteral(ch) && ch != EOF) {
+                str += ch;
+                ch = in.get();
+            }
+
+            if (str == "true" || str == "false") /* LITERAL_BOOL */;
+            else if (str == "nullptr") /* LITERAL_NULLPTR */;
+        }
     }
 
     /// SCANNER CODE END ///
